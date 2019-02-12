c4dffe3b8ce34d45a5ef1274fd39aadc982d0b6f
martensievers <hello@martensievers.com>
2019-02-12 13:05:04 +0100

    add class exercise

---
+++ setting-up-your-machine/script.js
@@ -0,0 +1,5 @@
+// Write your first 'Hello, World!' program
+console.log("Hello, World");
+
+// JavaScript is case-sensitive
+// Console.log("Hello, World");  // Throws an error
--- variables-and-statements/strings-and-numbers.js
+++ variables-and-statements/strings-and-numbers.js
@@ -1,1 +1,45 @@
-// Strings and Variables
+/***********************************************
+ * JavaScript Basics - Strings and Variables
+ */
+
+// Create a variable to store first name
+var firstName = "John";
+
+// Create a variable to store last name
+var lastName = "Doe";
+
+// Change the value of last name
+lastName = "Smith";
+
+// Create a variabel to combie first + last name
+var fullName = firstName + " " + lastName;
+
+// Print full name to the console
+console.log("Hello " + fullName + ".");
+
+
+
+/*********************************
+ * JavaScript Basics - Numbers
+ */
+
+// Create a number and assign it to a variable
+var age = 26;
+
+// Add
+var addResult = age + 1; // 27
+
+// Subtract
+var subtractResult = age - 26.5 // -0.5
+
+// Multiply
+var multiplyResult = age * 10 // 260
+
+// Divide
+var divideResult = age / 2 // 13
+
+// Parenthesis can be used to make sure that one operation happens before another
+var result = (age + 3) * 7 // 203
+
+// Print the result to the console
+console.log(result);
